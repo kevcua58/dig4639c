ed2953b497a6cd495eafd60b921ecb4923afc72b
Kevin Cua <kevcua58@knights.ucf.edu>
2019-01-23 23:59:36 -0500

    Added a greeting using React

---
+++ lab1/src/NameForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      field: {},
+      error: {}
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  //Function that validates form
+  validateForm() {
+    let field = this.state.field;
+    let error = {};
+    let validForm = true;
+
+    //Makes sure a name is entered
+    if (!field["name"]) {
+      validForm = false;
+      error["name"] = "Please enter a name.";
+    }
+
+    //Checks for letters only
+    if (typeof field["name"] !== "undefined") {
+      if (!field["name"].match(/^[A-Za-z ]*$/)) {
+        validForm = false;
+        error["name"] = "Please enter letters only.";
+      }
+    }
+
+    this.setState({
+      error:error
+    });
+    return validForm;
+  }
+
+  handleChange(event) {
+    let field = this.state.field;
+    field[event.target.name] = event.target.value;
+    this.setState({
+      field
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.validateForm()) {
+      let field = {};
+      field["name"] = "";
+      this.setState({field:field});
+      alert('Hello, ' + this.state.field.name + '!');
+    }
+  }
+
+  render() {
+    const style = {
+      color: 'red'
+    };
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={this.state.field.name} onChange={this.handleChange} />
+          <div style={style}>
+            {this.state.error.name}
+          </div>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default NameForm;
